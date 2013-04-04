8764303df9d20bdb89640ab4da1504d791353225
Kevin Cox <kevincox.ca@gmail.com>
2013-04-04 18:25:30 -0400

    Added test generator for live master branch.

--- genhtmltest.js
+++ genhtmltest.js
@@ -6,23 +6,30 @@ var path = require("path");
 var tests = fs.readdirSync("test/");
 
 var html = fs.readFileSync("test/test.html", "utf-8");
+var htmlweb = html;
 
 var mydir = path.dirname(module.filename)
 
 var mocha = path.dirname(require.resolve("mocha"));
-html = html.replace(/\{\{\{mocha\}\}\}/g, path.relative(mydir, mocha));
+html    = html.replace(/\{\{\{mocha\}\}\}/g, path.relative(mydir, mocha)+"/");
+htmlweb = htmlweb.replace(/\{\{\{mocha\}\}\}/g, "");
 
 var mocha = require.resolve("expect.js");
-html = html.replace(/\{\{\{expect\}\}\}/g, path.relative(mydir, mocha));
+html    = html.replace(/\{\{\{expect\}\}\}/g, path.relative(mydir, mocha));
+htmlweb = htmlweb.replace(/\{\{\{expect\}\}\}/g, "expect.js");
 
 var tf = "";
+var tfweb = "";
 tests.forEach(function(t)
 {
 	if ( t.slice(-3) != ".js" ) return;
-	
-	tf += '<script src="test/'+t+'"></script>\n';
+
+	tf    += '<script src="test/'+t+'"></script>\n';
+	tfweb += '<script src="https://raw.github.com/kevincox/url.js/master/test/'+t+'"></script>\n';
 });
-html = html.replace(/\{\{\{tests\}\}\}/g, tf);
+html    = html.replace(/\{\{\{tests\}\}\}/g, tf);
+htmlweb = htmlweb.replace(/\{\{\{tests\}\}\}/g, tfweb);
 
 fs.writeFileSync("test.html", html.replace(/\{\{\{url\}\}\}/g, "url.js"));
 fs.writeFileSync("test.min.html", html.replace(/\{\{\{url\}\}\}/g, "url.min.js"));
+fs.writeFileSync("test.web.html", htmlweb.replace(/\{\{\{url\}\}\}/g, "https://raw.github.com/kevincox/url.js/master/url.js"));
