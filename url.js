93e418925e06119a09a40e269b736ea6ddcc24f9
Kevin Cox <kevincox@kevincox.ca>
2013-11-13 09:54:26 -0500

    ws

--- url.js
+++ url.js
@@ -56,9 +56,9 @@ var array = /\[([^\[]*)\]$/;
  * take precedence over array properties, irrespective of where they are in the
  * query string.
  *
- * 	url.get("array[1]=test&array[foo]=bar",{array:true}).array[1]  === "test"
- * 	url.get("array[1]=test&array[foo]=bar",{array:true}).array.foo === "bar"
- * 	url.get("array=notanarray&array[0]=1",{array:true}).array      === "notanarray"
+ *     url.get("array[1]=test&array[foo]=bar",{array:true}).array[1]  === "test"
+ *     url.get("array[1]=test&array[foo]=bar",{array:true}).array.foo === "bar"
+ *     url.get("array=notanarray&array[0]=1",{array:true}).array      === "notanarray"
  *
  * If array parsing is enabled keys in the form of `name[]` will automatically
  * be given the next available index.  Note that this can be overwritten with
@@ -66,8 +66,8 @@ var array = /\[([^\[]*)\]$/;
  * two formats, although it is safe (and often useful) to add an automatic index
  * argument to the end of a query string.
  *
- * 	url.get("a[]=0&a[]=1&a[0]=2", {array:true})  -> {a:["2","1"]};
- * 	url.get("a[0]=0&a[1]=1&a[]=2", {array:true}) -> {a:["0","1","2"]};
+ *     url.get("a[]=0&a[]=1&a[0]=2", {array:true})  -> {a:["2","1"]};
+ *     url.get("a[0]=0&a[1]=1&a[]=2", {array:true}) -> {a:["0","1","2"]};
  *
  * @param{string} q The query string (the part after the '?').
  * @param{{full:boolean,array:boolean}=} opt Options.
@@ -86,29 +86,29 @@ var get = self["get"] = function(q, opt)
 	if ( typeof opt          == "undefined" ) opt = {};
 	if ( typeof opt["full"]  == "undefined" ) opt["full"] = false;
 	if ( typeof opt["array"] == "undefined" ) opt["array"] = false;
-
+	
 	if ( opt["full"] === true )
 	{
 		q = parse(q, {"get":false})["query"] || "";
 	}
-
+	
 	var o = {};
-
+	
 	var c = q.split("&");
 	for ( var i in c )
 	{
 		if (!c[i].length) continue;
-
+		
 		var d = c[i].indexOf("=");
 		var k = c[i], v = true;
 		if ( d >= 0 )
 		{
 			k = c[i].substr(0, d);
 			v = c[i].substr(d+1);
-
+			
 			v = decodeURIComponent(v);
 		}
-
+		
 		if (opt["array"])
 		{
 			var inds = [];
@@ -129,23 +129,23 @@ var get = self["get"] = function(q, opt)
 					//console.log("url.get: Array property "+curk+" already exists as string!");
 					return true;
 				}
-
+				
 				curo = curo[curk];
-
+				
 				if ( i === "" ) i = curo.length;
-
+				
 				curk = i;
 			})) continue;
 			curo[curk] = v;
 			continue;
 		}
-
+		
 		k = decodeURIComponent(k);
-
+		
 		//typeof o[k] == "undefined" || console.log("Property "+k+" already exists!");
 		o[k] = v;
 	}
-
+	
 	return o;
 };
 
@@ -204,9 +204,9 @@ var buildget = self["buildget"] = function(data, prefix)
 		var ek = encodeKey(k);
 		if ( typeof prefix != "undefined" )
 			ek = prefix+"["+ek+"]";
-
+		
 		var v = data[k];
-
+		
 		switch (typeof v)
 		{
 			case 'boolean':
@@ -274,9 +274,9 @@ var parse = self["parse"] = function(url, opt)
 {
 	var r = {}
 	if ( typeof opt == "undefined" ) opt = {};
-
+	
 	r["url"] = url;
-
+	
 	do {
 		var s0 = url.toLowerCase().match(scheme[0])
 		if ( s0 === null ) break;
@@ -287,67 +287,67 @@ var parse = self["parse"] = function(url, opt)
 		}
 		url = url.slice(s0[0].length);
 	} while (false);
-
+	
 	do {
 		var u = url.match(user)
 		if ( u === null ) break;
 		r["user"] = decodeURIComponent(u[1]);
-
+		
 		url = url.slice(u[1].length);
-
+		
 		do {
 			var p = url.match(pass)
 			if ( p === null ) break;
 			r["pass"] = decodeURIComponent(p[1]);
-
+			
 			url = url.slice(p[1].length+1); // +1 is for the ':'
 		} while (false);
-
+		
 		url = url.slice(1); // Drop the '@'.
 	} while (false);
-
+	
 	do {
 		var h = url.match(host)
 		if ( h === null ) break;
 		r["host"] = h[0];
-
+		
 		url = url.slice(h[0].length);
 	} while (false);
-
+	
 	do {
 		var p = url.match(port)
 		if ( p === null || p[1]==="" ) break;
 		r["port"] = parseInt(p[1]);
-
+		
 		url = url.slice(p[0].length);
 	} while (false);
-
+	
 	do {
 		var p = url.match(path)
 		if ( p === null ) break;
 		r["path"] = decodeURIComponent(p[0]);
-
+		
 		url = url.slice(p[0].length);
 	} while (false);
-
+	
 	do {
 		var q = url.match(query)
 		if ( q === null ) break;
 		r["query"] = q[1];
 		if ( opt["get"] !== false )
 			r["get"] = get(r["query"], opt["get"]);
-
+		
 		url = url.slice(q[0].length);
 	} while (false);
-
+	
 	do {
 		var h = url.match(hash)
 		if ( h === null ) break;
 		r["hash"] = decodeURIComponent(h[1]);
-
+		
 		//url = url.slice(h[0].length);
 	} while (false);
-
+	
 	return r;
 }
 
@@ -380,7 +380,7 @@ var noslash = ["mailto","bitcoin"];
 var build = self["build"] = function(data)
 {
 	var r = "";
-
+	
 	if ( typeof data["scheme"] != "undefined" )
 	{
 		r += data["scheme"];
@@ -410,7 +410,7 @@ var build = self["build"] = function(data)
 	{
 		r += data["path"];
 	}
-
+	
 	if ( typeof data["get"] != "undefined" )
 	{
 		r += "?" + buildget(data["get"]);
@@ -419,12 +419,12 @@ var build = self["build"] = function(data)
 	{
 		r += "?" + data["query"];
 	}
-
+	
 	if ( typeof data["hash"] != "undefined" )
 	{
 		r += "#" + data["hash"];
 	}
-
+	
 	return r || data["url"] || "";
 };
 
